bbf1fa4e753e8983534a15157a3639a2894dbbd6
Nick <82282004+sirvibegodlol@users.noreply.github.com>
2021-11-04 18:21:47 -0400

    Oahu
    Updates Oahu.h to actually have code. Updates the Test-Project Header file to have a tutorial on including the modules

--- src/Oahu.h
+++ src/Oahu.h
@@ -1,3 +1,14 @@
 // Oahu.h is the header file of all header files. If you would like to use the entirety of CLP then
 // #include "Oahu.h"
 // for full usage of everything CLP Related
+
+#include "Class.h"
+#include "Functions.h"
+#include "iostream.h"
+#include "Main.h"
+#include "Objects.h"
+#include "OC.h"
+#include "Return.h"
+#include "Cosmo/C_Alphabet.h"
+#include "Cosmo/C_NumericalSystem.h"
+#include "Cosmo/C_Special.h"
