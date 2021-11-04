b3eb988a2593c8f2d8ebe32f14a42387fa07c0f6
Nick <82282004+sirvibegodlol@users.noreply.github.com>
2021-11-04 09:18:44 -0400

    initial commit

---
+++ src/variables.h
@@ -0,0 +1,9 @@
+#pragma once
+
+// Everything in this file is used for declaring variables
+
+#define STRING(varName) string varName
+#define INT(varName) int varName
+#define CHAR(varName) char charName
+#define BOOLEAN(varName) bool varName
+#define FLOAT(varName) float varName
