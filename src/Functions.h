3dbb8e6262c1a39228eda4d2eeeabfb7f972b704
Nick <82282004+sirvibegodlol@users.noreply.github.com>
2021-11-04 09:45:38 -0400

    Update Functions.h

--- src/Functions.h
+++ src/Functions.h
@@ -1,1 +1,13 @@
+#pragma once
 
+// Allows for Dynamic Creation of functions in your programs
+
+#define PUBLIC_FUNCTION(funcName) public funcName()
+
+// Return Types
+
+#define INT_FUNCTION(funcName) int funcName()
+#define VOID_FUNCTION(funcName) void funcName()
+#define STRING_FUNCTION(funcName) string funcName()
+#define CHAR_FUNCTION(funcName) char funcName()
+#define FLOAT_FUNCTION(funcName) float funcName()
