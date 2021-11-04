5ffaeb3f6459246535a5f4559eb84aa0f8718fcf
Nick <82282004+sirvibegodlol@users.noreply.github.com>
2021-11-04 09:14:02 -0400

    Create iostream.h

---
+++ src/iostream.h
@@ -0,0 +1,5 @@
+#pragma once
+// The iostream(Input/Output Stream) is the Header that will be used whenever Logging to console is needed, or getting user input.
+
+#define LOG_NEWLINE(msg) std::cout << msg << std::endl;
+#define LOG(msg) std::cout << msg;
