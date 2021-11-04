19434b1d1ffcc4d8a27bef6ec90faba60cc0eae4
Nick <82282004+sirvibegodlol@users.noreply.github.com>
2021-11-04 09:15:14 -0400

    Update iostream.h

--- src/iostream.h
+++ src/iostream.h
@@ -1,5 +1,9 @@
 #pragma once
 // The iostream(Input/Output Stream) is the Header that will be used whenever Logging to console is needed, or getting user input.
 
+
+// LOG_NEWLINE Creates a new line after logging, LOG does not.
 #define LOG_NEWLINE(msg) std::cout << msg << std::endl;
 #define LOG(msg) std::cout << msg;
+
+#define GET_USER_INPUT std::cin.get():
