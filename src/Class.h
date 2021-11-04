f0d9e7afe1a7d7c3b5676f25e47ef44b3a595187
Nick <82282004+sirvibegodlol@users.noreply.github.com>
2021-11-04 09:40:59 -0400

    Create Class.h

---
+++ src/Class.h
@@ -0,0 +1,8 @@
+#pragma once
+
+// The Class System allows for dynamic creation of Classes in your programm
+
+#define DYNAMIC_CLASS(className) class className
+#define PUBLIC public:
+#define PRIVATE private:
+#define PROTECTED protected:
