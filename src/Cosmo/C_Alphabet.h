adec91bd99118fba76818be99606cd156beecba2
Nick <82282004+sirvibegodlol@users.noreply.github.com>
2021-11-04 09:31:36 -0400

    Create C_Alphabet.h

---
+++ src/Cosmo/C_Alphabet.h
@@ -0,0 +1,107 @@
+#pragma once
+
+// The Cosmo Alphabet has all letters excluding special characters, if your looking for ! or ? go to C_Special
+
+// A
+#define LOWER_a a
+#define UPPER_A A
+
+// B
+#define LOWER_B b
+#define UPPER_B b
+
+// C
+#define LOWER_C c
+#define UPPER_C C
+
+// D
+#define LOWER_D d
+#define UPPER_D D
+
+// E
+#define LOWER_E e
+#define UPPER_E E
+
+// F
+#define LOWER_F f
+#define UPPER_F F
+
+// G
+#define LOWER_G g
+#define UPPER_G G
+
+// H
+#define LOWER_H h
+#define UPPER_H H
+
+// I
+#define LOWER_I i
+#define UPPER_I I
+
+// J
+#define LOWER_J j
+#define UPPER_J J
+
+// K
+#define LOWER_K k
+#define UPPER_K K
+
+// L
+#define LOWER_L l
+#define UPPER_L L
+
+// M
+#define LOWER_M m
+#define UPPER_M M
+
+// N
+#define LOWER_N n
+#define UPPER_N N
+
+// O
+#define LOWER_O o
+#define UPPER_O O
+
+// P
+#define LOWER_P p
+#define UPPER_P P
+
+// Q
+#define LOWER_Q q
+#define UPPER_Q Q
+
+// R
+#define LOWER_R r
+#define UPPER_R R
+
+// S
+#define LOWER_S s
+#define UPPER_S S
+
+// T
+#define LOWER_T t
+#define UPPER_T T
+
+// U
+#define LOWER_U u
+#define UPPER_U U
+
+// V
+#define LOWER_V v
+#define UPPER_V V
+
+// W
+#define LOWER_W w
+#define UPPER_W W
+
+// X
+#define LOWER_X x
+#define UPPER_X X
+
+// Y
+#define LOWER_Y y
+#define UPPER_Y y
+
+// z
+#define LOWER_Z z
+#define UPPER_Z Z
