c97808f8ca6fcd2e56c231798dcafc5bca0ca89a
Nick <82282004+sirvibegodlol@users.noreply.github.com>
2021-11-04 09:10:56 -0400

    Update and rename closers.h to src/OC.h

--- closers.h
+++
@@ -1,4 +0,0 @@
-// Include this file to gain access to { and }
-
-#define OPEN_CURLY {
-#define CLOSED_CURLY }
---
+++ src/OC.h
@@ -0,0 +1,9 @@
+// Include this file to gain access to { and } also ( and ) also [ and ]
+
+#define OPEN_CURLY {
+#define CLOSED_CURLY }
+#define OPEN_PARENTHESE (
+#define CLOSED_PARENTHESE )
+#define OPEN_SQUARE_BRACKET [
+#define CLOSED_SQUARE_BRACKET ]
+
