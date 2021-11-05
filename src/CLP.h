55f57b76908521c95cf4d4630de1d1d49f184739
Nick <82282004+sirvibegodlol@users.noreply.github.com>
2021-11-05 08:23:47 -0400

    Update and rename Oahu.h to CLP.h

--- src/CLP.h
+++ src/CLP.h
@@ -1,5 +1,5 @@
-// Oahu.h is the header file of all header files. If you would like to use the entirety of CLP then
-// #include "Oahu.h"
+// CLP.h is the header file of all header files. If you would like to use the entirety of CLP then
+// #include "CLP.h"
 // for full usage of everything CLP Related
 
 #include "Class.h"
