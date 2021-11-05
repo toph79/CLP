6f10b07cbb48ce3d66d279b0987a6a246c085251
Nick <82282004+sirvibegodlol@users.noreply.github.com>
2021-11-05 09:38:58 -0400

    Create Arguments.h

---
+++ src/Arguments.h
@@ -0,0 +1,7 @@
+// Stuff involving class or function arguments
+
+#define COMMA ,
+#define IS_EQUAL == 
+#define NOT_EQUAL !=
+#define INCREMENT(varToIncrement) varToIncrement++
+#define DECREMENT(varToDecrement) varToDecrement--
